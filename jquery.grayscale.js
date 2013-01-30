870c4d41cff99e90627ea9440d93c5fdf1fc25cb
Fernando Cezar Chaves <fernandocchaves@gmail.com>
2013-01-30 10:16:40 -0200

    Grayscale

---
+++ jquery.grayscale.js
@@ -0,0 +1,68 @@
+(function( $ ){
+    $.fn.grayscale = function() {
+        return this.each(function() {
+            var el = $(this);
+			el.css({
+				"position" : "absolute"
+			}).wrap("<div class='img_wrapper' style='display: inline-block'>").clone().addClass('img_grayscale').css({
+				"position" : "absolute",
+				"z-index" : "98",
+				"opacity" : "0"
+			}).insertBefore(el).queue(function() {
+				var el = $(this);
+
+				el.parent().css({
+					"width" : this.width,
+					"height" : this.height
+				});
+				el.dequeue();
+
+			});
+			this.src = canvasgrayscale(this.src);
+        });
+ 
+    }; 
+})( jQuery );
+
+// Grayscale w canvas method
+function canvasgrayscale(src) {
+
+	var canvas = document.createElement('canvas');
+
+	var ctx = canvas.getContext('2d');
+
+	var imgObj = new Image();
+
+	imgObj.src = src;
+
+	canvas.width = imgObj.width;
+
+	canvas.height = imgObj.height;
+
+	ctx.drawImage(imgObj, 0, 0);
+
+	var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+	for(var y = 0; y < imgPixels.height; y++) {
+
+		for(var x = 0; x < imgPixels.width; x++) {
+
+			var i = (y * 4) * imgPixels.width + x * 4;
+
+			var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+
+			imgPixels.data[i] = avg;
+
+			imgPixels.data[i + 1] = avg;
+
+			imgPixels.data[i + 2] = avg;
+
+		}
+
+	}
+
+	ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+
+	return canvas.toDataURL();
+
+}
